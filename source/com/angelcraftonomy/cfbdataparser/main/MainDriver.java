6d1b106fcbabfae37edf1c817f88e13ed12c148f
Darrell Best <dsbest@g.clemson.edu>
2017-03-14 18:44:17 -0400

    Fixing initial commit

---
+++ source/com/angelcraftonomy/cfbdataparser/main/MainDriver.java
@@ -0,0 +1,15 @@
+package com.angelcraftonomy.cfbdataparser.main;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class MainDriver {
+
+	public static void main(String[] args) {
+		String html = "<html><head><title>First parse</title></head>"
+				+ "<body><p>Parsed HTML into a doc.</p></body></html>";
+		Document doc = Jsoup.parse(html);
+		System.out.println(doc);
+	}
+
+}
